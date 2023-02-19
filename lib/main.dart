7dcd996e54e3a0b00d85cfa455369490daa214c3
geuntae.kim <kok617@naver.com>
2023-02-19 21:08:04 +0900

    Ref: ApiService static 으로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home_screen.dart';
-import 'package:toonflix/services/api_service.dart';
 
 void main() {
-  // HTTP TEST
-  ApiService().getTodaysToons();
-
   runApp(const App());
 }
 
